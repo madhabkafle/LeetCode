55dddd4145529f458ccc9fba17072a288f767510
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-14 01:02:58 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -13,21 +13,43 @@ class Solution{
     int cutRod(int price[], int n) {
         int l=n;
         vector<vector<int>> dp(n+1,vector<int>(l+1,-1));
-       return maxval(price,n-1,l,dp);
+       return maxval(price,n,l);
     }
-    int maxval(int price[], int ind,int l,vector<vector<int>> &dp){
-        if(ind==0){
-            return (l*price[0]); //lenght of the material left * value of that material
-    }
-    if(dp[ind][l]!=-1) return dp[ind][l];
-    int not_pick=0+ maxval(price,ind-1,l,dp);
+    //tabulation
+    int maxval(int price[], int ind,int l){
+        vector<vector<int>> dp(ind,vector<int>(l+1,0));
+        for(int i=0;i<=l;i++){
+            dp[0][i]=i*price[0];
+        }
+     
+    for(int i=1;i<ind;i++){
+        for(int j=0;j<=l;j++){
+         int not_pick=0+ dp[i-1][j];
     int pick=INT_MIN;
-    int rodlength=ind+1;
-    if(rodlength<=l)
-    pick=price[ind]+maxval(price,ind,l-rodlength,dp);
+    int rodlength=i+1;
+    if(rodlength<=j)
+    pick=price[i]+dp[i][j-rodlength];
     
-    return dp[ind][l]=max(pick,not_pick);
+    dp[i][j]=max(pick,not_pick);   
+        }
+       
     }
+     return dp[ind-1][l];
+    }
+    //memo
+    // int maxval(int price[], int ind,int l,vector<vector<int>> &dp){
+    //     if(ind==0){
+    //         return (l*price[0]); //lenght of the material left * value of that material
+    // }
+    // if(dp[ind][l]!=-1) return dp[ind][l];
+    // int not_pick=0+ maxval(price,ind-1,l,dp);
+    // int pick=INT_MIN;
+    // int rodlength=ind+1;
+    // if(rodlength<=l)
+    // pick=price[ind]+maxval(price,ind,l-rodlength,dp);
+    
+    // return dp[ind][l]=max(pick,not_pick);
+    // }
 };
 
 //{ Driver Code Starts.
