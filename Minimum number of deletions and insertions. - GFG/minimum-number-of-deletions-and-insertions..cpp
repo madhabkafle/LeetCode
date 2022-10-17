ba84fd42769ef27ab60acb7c5a1c7582de524edb
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-18 00:55:58 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -17,13 +17,26 @@ class Solution{
 	  return diff;
 	    
 	}
-	int fun(string str1, string str2, int n,int m,vector<vector<int>> &dp){
-	    if(n<=0 || m<=0)return 0;
-	    if(dp[n][m]!=-1)return dp[n][m];
-	    if(str1[n-1]==str2[m-1])return 1+fun(str1,str2,n-1,m-1,dp);
-	    
-	    return dp[n][m]=max(fun(str1,str2,n-1,m,dp),fun(str1,str2,n,m-1,dp));
-	    
+	int fun(string s1,string s2,int n,int m,vector<vector<int>> &dp){
+	   for(int i=0;i<=n;i++)
+       {
+           dp[i][0]=0;
+       }
+        for(int j=0;j<=m;j++)
+       {
+           dp[0][j]=0;
+       }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(s1[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+        }
+        else
+        dp[i][j]=0+max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
 	}
 };
 
