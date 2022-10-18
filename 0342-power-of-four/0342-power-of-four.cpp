8adfb828773a4268e615f1e5a23e93dfcf4a0b97
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-18 22:47:09 +0530

    Time: 3 ms (57.76%), Space: 6.2 MB (10.00%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
+   
     bool isPowerOfFour(int n) {
        if(n<=0)return false;
-        double x = log10(n)/log10(4);
-      cout<<x<<"->"<<(int)x<<endl;
-     if(x==(int)x)
-         return true;
-     return false;
+    double x=log10(n)/log10(4);
+    if(x==(int)x)return true;
+    return false;
     }
 };
