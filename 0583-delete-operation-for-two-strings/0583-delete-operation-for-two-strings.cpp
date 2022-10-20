40a5f29487d93d7aa4918a3b5cee9ec0c11b50d3
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-20 23:57:40 +0530

    Time: 56 ms (29.58%), Space: 12.3 MB (19.07%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -3,18 +3,27 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         int l= helper(n,m,word1,word2,dp);
+        cout<<l;
         int ans=(n-l)+(m-l);
         return ans;
     }
-    int helper(int i,int j,string s1,string s2,vector<vector<int>>&dp){
-        if(i==0 || j==0){
-            return 0;
+    int helper(int n,int m,string s1,string s2,vector<vector<int>>&dp){
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i-1]==s2[j-1])return dp[i][j]=1+helper(i-1,j-1,s1,s2,dp);
-        
-        return dp[i][j]=max(helper(i,j-1,s1,s2,dp), helper(i-1,j,s1,s2,dp));
+        for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+        if(s1[i-1]==s2[j-1])
+        dp[i][j]=1+dp[i-1][j-1];
+        else
+        dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+       return dp[n][m];
     }
 };
