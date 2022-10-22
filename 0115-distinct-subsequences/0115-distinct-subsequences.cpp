8e8d56ba70873a5a16958a044a36985cd4abc3cc
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-22 13:29:08 +0530

    Time: 111 ms (12.68%), Space: 30.1 MB (16.77%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -12,9 +12,7 @@ public:
         for(int i=0;i<=n;i++){
             dp[i][0]=1;
         }
-        for(int j=1;j<=m;j++){
-            dp[0][j]=0;
-        }
+        
      for(int i=1;i<=n;i++){
          for(int j=1;j<=m;j++){
              if(s[i-1]==t[j-1]){
