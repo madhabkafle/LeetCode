7f5491b3254db6633ea8943d57294c8c0bf2b1ce
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-23 21:00:44 +0530

    Time: 44 ms (26.97%), Space: 9 MB (40.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,18 +3,28 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         int ans= fun(n,m,word1,word2,dp);
         return ans;
     }
-    int fun(int i,int j,string s1,string s2,vector<vector<int>> &dp){
-        if(i<=0) return j;
-        if(j<=0) return i;
-        if(s1[i-1]==s2[j-1])return 0+ fun(i-1,j-1,s1,s2,dp);
-        if(dp[i][j]!=-1)return dp[i][j];
-        int insertion= 1+ fun(i,j-1,s1,s2,dp); //insert
-        int deletion= 1+fun(i-1,j,s1,s2,dp);  //delete
-        int replace= 1+fun(i-1,j-1,s1,s2,dp); //replace
-        return dp[i][j]=min({insertion,deletion,replace});
+    int fun(int n,int m,string s1,string s2,vector<vector<int>> &dp){
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+             if(s1[i-1]==s2[j-1])dp[i][j]= 0 + dp[i-1][j-1];
+        else{
+        int insertion= 1+ dp[i][j-1]; //insert
+        int deletion= 1+dp[i-1][j];//delete
+        int replace= 1+dp[i-1][j-1]; //replace
+        dp[i][j]=min({insertion,deletion,replace});
+        }
+            }
+        }
+        return dp[n][m];
     }
 };
