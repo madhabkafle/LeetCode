3be9e9cdeff133c96c2ca05c6eda6d36c470cfe5
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-10-28 10:39:27 +0530

    Time: 7 ms (98.46%), Space: 10.4 MB (71.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -29,16 +29,18 @@ public:
          vector<int> temp;
          int n=nums.size();
          temp.push_back(nums[0]);
-         int len=0;
+         int len=1;
          for(int i=1;i<n;i++){
            
-             if(nums[i]>temp.back())
+             if(nums[i]>temp.back()){
                  temp.push_back(nums[i]);
+                 len++;
+             }
              else{
                  int ind= lower_bound(temp.begin(),temp.end(),nums[i])- temp.begin();
                  temp[ind]=nums[i];
              }
          }
-         return len=temp.size();
+         return len;
     }
 };
