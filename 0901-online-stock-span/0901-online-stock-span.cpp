458f6b63a22f9c5e39cf7a253c49a279d8328c67
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-11-09 23:14:03 +0530

    Time: 252 ms (84.38%), Space: 84.4 MB (22.76%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,13 +1,13 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>> st;
+   stack<pair<int,int>> st;
     StockSpanner() {
-        
+     
     }
     
     int next(int price) {
         int count=1;
-        while(!st.empty() && st.top().first<=price){
+        while(!st.empty()&& st.top().first<=price){
             count+=st.top().second;
             st.pop();
         }
