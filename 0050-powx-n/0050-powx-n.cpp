86b799993fce7e5250ed2ac66df99da9c68924a9
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-12-13 15:54:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (23.90%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
-    }
+   double power(double a,int b){
+     if(b==0)return 1;
+	else if(b%2==0)return power(a*a,b/2);
+	else return a*power(a*a,b/2);
+	}        
+
+	double myPow(double x, int n) {
+	if(n<0)
+	{
+		n=abs(n);
+		x=1/x;
+	}
+	return power(x,n);
+   }
 };
