fc49ad8f06fc54c8233ca187578b736bf4233a2f
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-12-15 23:29:16 +0530

    Time: 3 ms (89.12%), Space: 15.8 MB (14.38%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    void solve(int open, int close, string op ,vector<string> &v)
-    {
-        if(open==0 && close==0)
-        { 
-            v.push_back(op);
-            return;            
-        }
-        if(open!=0)
-        {
-            string op1 = op;
-            op1.push_back('(');
-            solve(open-1, close, op1, v);
-               
-        }
-        if(close>open)
-        {
-            string op2 = op;
-            op2.push_back(')');
-                solve(open, close-1, op2, v);
-       
-        }
-        return;
-           
-    }
-    
     vector<string> generateParenthesis(int n) {
-        vector<string> v;
-        int close = n;
-        int open = n;
-        string op = "";
-        solve(open, close, op, v);
-        return v;
+        string op="";
+      vector<string> str;
+      fun(str,op,n,n);
+      return str;
     }
+  void fun(vector<string> &str,string op,int open,int close){
+    if(open==0 && close==0){
+      str.push_back(op);
+      return;
+    }
+    if(open!=0){
+      string op1=op;
+      op1.push_back('(');
+      fun(str,op1,open-1,close);
+    }
+    // if(open==close){
+    //   string op2=op;
+    //   op2.push_back("(");
+    //   fun(str,op2,open-1,close);
+    // }
+    if(open<close){
+       string op2=op;
+       op2.push_back(')');
+      fun(str,op2,open,close-1);
+    }
+  }
 };
