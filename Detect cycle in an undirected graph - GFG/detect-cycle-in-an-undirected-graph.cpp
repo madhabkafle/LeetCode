ed5950970c0b52cb4d927871418d2fc62466a0b4
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2022-12-25 13:49:22 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+      bool detect(int src,vector<int> adj[], int visi[]){
+          visi[src]=1;
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{src,-1},0});
+        while(!q.empty()){
+            int node=q.front().first.first;
+            int parent= q.front().first.second;
+            int vis=q.front().second;
+            q.pop();
+            for(auto it: adj[node]){
+             if(!visi[it]){
+             visi[it]=1;
+             q.push({{it,node},visi[it]});
+            }
+            else if(parent != it){
+                return true;
+            }
+           
+            }
+        }
+        return false;
+      }
+      bool isCycle(int V, vector<int> adj[]) {
+       int visi[V]={0};
+       for(int i=0;i<V;i++){
+           if(!visi[i]){
+               if(detect(i,adj,visi)==true)
+               return true;
+           }
+       }
+       return false;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
