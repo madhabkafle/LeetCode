5a4b387e765c6d0866f7e8f4d34e3f050307fa03
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-01-08 15:44:47 +0530

    Time: 35 ms (88.89%), Space: 12.5 MB (66.56%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -3,7 +3,6 @@ public:
     int maxPoints(vector<vector<int>>& pt) {
         int ans=1; 
         int n=pt.size();
-       // slope of every point with selected point
         for(int i=0;i<n-1;i++){
             map<double,int> mp;
             for(int j=i+1;j<n;j++){
@@ -14,10 +13,9 @@ public:
                     mp[x]++;
                 }
             }
-            // same slope w.r.t.that selected point
             int temp = 0;
             for(auto it:mp)
-                temp = max(temp, it.second+1);    // +1 for the current point(point itself)
+                temp = max(temp, it.second+1); 
             ans = max(temp, ans);
         }
         return ans;
