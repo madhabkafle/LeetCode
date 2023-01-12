f0e66a98d2dba3a4d7ad71c79f413fe47ce490d2
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-01-12 19:24:15 +0530

    Time: 256 ms (58.61%), Space: 73.4 MB (21.19%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-  int dfs(int curr,int parent,vector<vector<int>>& edges,vector<bool>& has,unordered_map<int,vector<int>> &adj){
+  int dfs(int curr,int parent,vector<vector<int>>& edges, vector<bool>& has,unordered_map<int,vector<int>> &adj){
     int time=0;
-    for(auto &x: adj[curr]){
-      if(x==parent){
+    for(auto &it: adj[curr]){
+      if(it==parent)
         continue;
-      }
-      int time_to_pick = dfs(x,curr,edges,has,adj);
-      if(time_to_pick > 0 || has[x]==true)
-        time+=time_to_pick +2;
+      
+      int time_to_pick= dfs(it,curr,edges,has,adj);
+      if(time_to_pick>0||has[it]==true)
+        time+=time_to_pick+2;
     }
     return time;
   }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& has) {
-       unordered_map<int,vector<int>> adj;
-      for(auto &vec: edges){
-        int u= vec[0];
-        int v= vec[1];
+        unordered_map<int,vector<int>> adj;
+      for(auto x: edges){
+        int u=x[0];
+        int v=x[1];
         adj[u].push_back(v);
         adj[v].push_back(u);
       }
