560d3640747284e8ca61639851fc8d9c29096259
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-01-16 22:23:27 +0530

    Time: 17 ms (81.26%), Space: 16.9 MB (92.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newy) {
      int i=0;
+      vector<vector<int>> ans;
+        
       while(i<intervals.size()){
         if(intervals[i][1]<newy[0]){
+        ans.push_back({intervals[i]});
           i++;
         }
         else if(intervals[i][0]>newy[1]){
-          intervals.insert(intervals.begin()+i,newy);
-          return intervals;
+          break;
         }
         else{
           newy[0]=min(intervals[i][0],newy[0]);
           newy[1]=max(intervals[i][1],newy[1]);
-          intervals.erase(intervals.begin()+i);
-          
+          i++;
         }
       }
-      intervals.push_back(newy);
-      return intervals;
+      ans.push_back(newy);
+      while(i<intervals.size()){
+        ans.push_back({intervals[i]});
+        i++;
+      }
+      return ans;
     }
 };
