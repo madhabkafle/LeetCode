301a5ba7846ffe6b98d7dc22097b406d4658f536
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-02-18 12:39:25 +0530

    Time: 86 ms (5.18%), Space: 223.9 MB (5.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i=0,n=s.size();
+        stack<char> st;
+      string c="";
       string str="";
-      string result="";
-      int x=n-1;
-        string k=s;
-      while(k[x]==' '){
-        k.pop_back();
-        x--;
-      }
-      n=k.size();
-      while(i<n){
-        while(i<n &&k[i]==' '){
-          i++;
+      st.push(',');
+      for(int i=0;i<s.size();i++){
+        if(s[i]!=' '){
+          st.push(s[i]);
         }
-        while(i<n && k[i]!=' '){
-          str.push_back(k[i]);
-          i++;
-        }
-        result = str + " " +result;
-        
-        str.clear();
-        i++;
+        else
+          st.push(',');
       }
-      result.pop_back();
-      return result;
+      while(!st.empty()){
+        char ch=st.top();
+        st.pop();
+        if(ch!=','){
+          c+=ch;
+        }
+        else if(ch==','){
+          reverse(c.begin(),c.end());
+          str=str+c+' ';
+          c="";
+        }
+      }
+      string ans="";
+      int i=0;
+        if(str[0]==' '){
+          while(str[i]==' '){
+            i++;
+          }
+          }
+      int j=str.size()-1;
+      if(str[j]==' '){
+        while(str[j]==' '){
+          j--;
+        }
+      }
+      while(i<=j){
+            ans+=str[i];
+            i++;
+      }
+      int k=0;
+      string sans="";
+      while(k<ans.size()){
+        int z=k;
+        if(ans[k]!=' '){
+          sans+=ans[k];
+          k++;
+        }
+        else if(ans[k]==' '){
+          z=k+1;
+          sans+=' ';
+          while(ans[z]==' '){
+            z++;
+          }
+          k=z;
+        }
+      }
+      return sans;
     }
 };
