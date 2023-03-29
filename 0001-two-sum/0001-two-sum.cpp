ea60947f8c68cf775a03fea2507c2b255e68d9b6
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-03-29 09:43:43 +0530

    Time: 12 ms (80.76%), Space: 11 MB (27.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,14 +3,17 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
         int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+                break;
             }
+            else{
+                mp[nums[i]]=i;
+            }
+            
         }
         return ans;
     }
