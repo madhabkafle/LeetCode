5c42349cbd28cb8fb9152d7923edb09bb59b4a8a
Madhab Kafle <73248000+madhabkafle@users.noreply.github.com>
2023-06-18 19:50:54 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        vector<int> dis(N,1e9);
+        vector<int> adj[N];
+        for(auto i:edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        queue<int> q;
+        dis[src]=0;
+        q.push(src);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it: adj[node]){
+                if(dis[it]>dis[node]+1){
+                dis[it]=dis[node]+1;
+                q.push(it);
+                }
+            }
+        }
+        vector<int> ans(N,-1);
+        for(int i=0;i<N;i++){
+            if(dis[i]!=1e9)
+            ans[i]=dis[i];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for(int j=0; j<2; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src; cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(edges, n, m, src);
+
+        for (auto x : res){
+            cout<<x<<" ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
